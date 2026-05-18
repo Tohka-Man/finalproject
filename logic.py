50158ed96a32d145b65c6e5079cd12cf25eeb881
Tohka-Man <a_avtaev10@mail.ru>
2026-05-18 15:10:31 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -4,6 +4,8 @@ from config import database
 import os
 from typing import List, Tuple, Optional, Any
 
+specialists = [ (_,) for _ in (['Программист', 'Отдел продаж'])]
+
 class DatabaseManager:
     def __init__(self, database):
         self.database = database
@@ -11,13 +13,13 @@ class DatabaseManager:
     def create_tables(self):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute('''
+            """conn.execute('''
             CREATE TABLE IF NOT EXISTS st_questions (
                 id INTEGER PRIMARY KEY,
                 question TEXT
                 answer TEXT
             )
-        ''')
+        ''')"""
 
             conn.execute('''
             CREATE TABLE IF NOT EXISTS specialists (
@@ -37,3 +39,38 @@ class DatabaseManager:
         ''')
 
             conn.commit()
+
+    def __executemany(self, sql, data):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.executemany(sql, data)
+            conn.commit()
+
+    def __select_data(self, sql, data = tuple()):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute(sql, data)
+            return cur.fetchall()
+        
+    def default_insert(self):
+        sql = 'INSERT OR IGNORE INTO specialists (specialist) values(?)'
+        data = specialists
+        self.__executemany(sql, data)
+
+    def insert_quest(self, data):
+        sql = 'INSERT OR IGNORE INTO spec_questions (question, specialist) values(?, ?)'
+        self.__executemany(sql, data)
+
+
+    
+
+
+
+
+
+
+
+
+
+
