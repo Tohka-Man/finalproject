7a3ef0da60056851149b694c096958185b45ede8
Tohka-Man <a_avtaev10@mail.ru>
2026-05-18 15:10:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,22 @@
 import telebot, os
 from config import *
 from logic import *
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 bot = telebot.TeleBot(token)
+
+
+
+cancel_button = "Отмена 🚫"
+def gen_markup(rows):
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row_width = 1
+    for row in rows:
+        markup.add(KeyboardButton(row))
+    markup.add(KeyboardButton(cancel_button))
+    return markup
+
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -53,3 +65,38 @@ def send_welcome(message):
 def send_welcome(message):
     bot.send_message(message.chat.id, ''' Информацию о доставке вы можете найти на странице оформления заказа на нашем сайте. Там указаны доступные способы доставки и сроки.
 ''')
+
+
+@bot.message_handler(commands=['special'])
+def get_question(message):
+    bot.send_message(message.chat.id, "Введите вопрос:")
+    bot.register_next_step_handler(message, name_q)
+
+
+def name_q(message):
+    question = message.text
+    data=[question]
+    specialists = [x[0] for x in manager.get_specialist()] #тоже самое в лоджик
+    bot.send_message(message.chat.id, "Введите специалиста", reply_markup=gen_markup(specialists))
+    bot.register_next_step_handler(message, callback_quest, data=data, specialists=specialists)
+
+
+def callback_quest(message, data, statuses):
+    specialist = message.text
+    if message.text == cancel_button:
+        specialist(message)
+        return
+    if specialist not in specialists:
+        bot.send_message(message.chat.id, "Ты выбрал специалиста не из списка, попробуй еще раз!)",
+        reply_markup=gen_markup(statuses))
+        bot.register_next_step_handler(message, callback_quest, data=data, specialists=specialists)
+        return
+    specialist = manager.get_specialist_id(specialist) #доработать get_specialist_id в лоджик
+    data.append(specialist)
+    manager.insert_quest([tuple(data)])
+    bot.send_message(message.chat.id, "Вопрос сохранен")
+
+
+if __name__ == '__main__':
+    manager = DatabaseManager(database)
+    bot.infinity_polling()
